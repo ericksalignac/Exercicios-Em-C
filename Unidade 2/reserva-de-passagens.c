be87ff22980f4280f00fe9767f5befee4c5f52c9
ericksalignac <ericksalignac@gmail.com>
2022-03-02 10:17:47 -0300

    Correção dos caracteres de saída

--- Unidade 2/reserva-de-passagens.c
+++ Unidade 2/reserva-de-passagens.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
     int mat [3] [5] = {
@@ -11,12 +13,12 @@ int main(int argc, char const *argv[])
     int reservou;
 
     do
-    {printf ("\n - Reservar voo \2 - Sair \nInforme a opção desejada");
+    {printf ("\n 1 -Reservar voo \n 2 -Sair \n\nInforme a opcao desejada: ");
     scanf("%d", &opcao);
     switch (opcao)
     {
     case 1 :
-        printf("Informe o número do voo (1-3)");
+        printf("\nInforme o numero do voo (1-3): ");
         scanf("%d", &numVoo);
         reservou = 0;
         if (numVoo >= 1 && numVoo <=3) {
@@ -30,21 +32,21 @@ int main(int argc, char const *argv[])
 
             if (reservou == 1)
             {
-                printf ("Seu voo foi reservado com sucesso!");
+                printf ("\nSeu voo foi reservado com sucesso!");
             } else {
-                printf ("Não há poltronas disponíves nesse voo.");
+                printf ("\nNao ha poltronas disponives nesse voo.");
             }
             
-        } else  {printf("Opção inválida");
+        } else  {printf("\nOpcao invalida");
         }
         break;
     
     case 2:
-        printf("Saíndo...");
+        printf("\nSaindo...");
         break;
 
     default:
-        printf("Opção inválida!");
+        printf("\nOpcao invalida!");
     }
     } while (opcao !=2);
           return 0;
